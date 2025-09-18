544f293c987fd0b5e9d3c17d7e1d39e1c183ecc2
Yusuf Manshur <ymanshur@gmail.com>
2025-09-18 14:58:04 +0700

    feat(config/application): set Asia/Jakarta as local timezone

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module SleepbookDemo
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Jakarta"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
