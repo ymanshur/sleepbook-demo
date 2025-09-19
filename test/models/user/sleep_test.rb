273e448fe07824b4331a981011df26844a35a79e
Yusuf Manshur <ymanshur@gmail.com>
2025-09-19 23:18:36 +0700

    test(sleep): duration must not nil

--- test/models/user/sleep_test.rb
+++ test/models/user/sleep_test.rb
@@ -5,4 +5,11 @@ class User::SleepTest < ActiveSupport::TestCase
     user_sleep = User::Sleep.new
     assert_not user_sleep.save, "Saved the user sleep without start_time"
   end
+
+  test "should set user sleep duration" do
+    user = users(:one)
+    user_sleep = User::Sleep.new(user:, start_time: Time.current)
+    user_sleep.save
+    assert_not_nil user_sleep.duration, "Saved the user sleep with nil duration"
+  end
 end
