70cf61ff3a9d3a0de5fa408ad107be80cbef179c
Yusuf Manshur <ymanshur@gmail.com>
2025-09-19 20:37:01 +0700

    feat(response_handler): add meta to success response

--- app/controllers/concerns/response_handler.rb
+++ app/controllers/concerns/response_handler.rb
@@ -1,10 +1,6 @@
 module ResponseHandler
   extend ActiveSupport::Concern
 
-  def json_response(options = {}, status = :internal_server_error)
-    render json: JsonResponse.new(options), status:
-  end
-
   def render_error_response(error, status = :unprocessable_entity, message = "")
     json_response({
                     success: false,
@@ -13,11 +9,18 @@ module ResponseHandler
                   }, status)
   end
 
-  def render_success_response(data: {}, message: "", status: :ok)
+  def render_success_response(data: {}, message: "", status: :ok, meta: {})
     json_response({
                     success: true,
                     message:,
-                    data:
+                    data:,
+                    meta:
                   }, status)
+  end
+
+  private
+
+  def json_response(options = {}, status = :internal_server_error)
+    render json: JsonResponse.new(options), status:
   end
 end
