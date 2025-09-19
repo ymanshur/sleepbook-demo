0297668594eb58c60953c5bb559b565fc0918eb7
Yusuf Manshur <ymanshur@gmail.com>
2025-09-20 06:23:02 +0700

    test(sleep): should not save sleep withour user

--- test/models/user/sleep_test.rb
+++ test/models/user/sleep_test.rb
@@ -1,24 +1,35 @@
 require "test_helper"
 
 class User::SleepTest < ActiveSupport::TestCase
-  test "should not save user sleep without start_time" do
+  test "should not save sleep without user" do
     user_sleep = User::Sleep.new
-    assert_not user_sleep.save, "Saved the user sleep without start_time"
+
+    assert_not user_sleep.save, "Saved the sleep without user"
   end
 
-  test "should set user sleep duration" do
-    user = users(:one)
-    user_sleep = User::Sleep.new(user:, start_time: Time.current)
-    user_sleep.save
-    assert_not_nil user_sleep.duration, "Saved the user sleep with nil duration"
+  test "should not save sleep without start_time" do
+    user_sleep = User::Sleep.new
+
+    assert_not user_sleep.save, "Saved the sleep without start_time"
   end
 
-  test "should set user sleep duration when end_time present" do
+  test "should not save sleep with nil duration" do
     user = users(:one)
-    tnow = Time.current
-    duration = 8.hours
-    user_sleep = User::Sleep.new(user:, start_time: tnow, end_time: tnow + duration)
-    user_sleep.save
-    assert_equal duration, user_sleep.duration, "Saved the user sleep with uncorrect duration"
+
+    user_sleep = User::Sleep.create!(user:, start_time: Time.current)
+
+    assert_not_nil user_sleep.duration, "Saved the sleep with nil duration"
+  end
+
+
+  test "should not completed the sleep without correct duration" do
+    user = users(:one)
+
+    user_sleep = User::Sleep.create!(user:, start_time: Time.current)
+
+    duration = 6.hours
+    user_sleep.update!(end_time: user_sleep.start_time + duration)
+
+    assert_equal duration, user_sleep.duration, "Completed the sleep without correct duration"
   end
 end
