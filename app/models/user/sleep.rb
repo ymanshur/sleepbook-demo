91fcc2fdb40761e6b545d6e15a28baacf6f76e7c
Yusuf Manshur <ymanshur@gmail.com>
2025-09-20 05:36:18 +0700

    feat(sleep): add set duration callback before save
    The callback will respond to any changes in other attributes including the `duration` attribute. To ensure the duration is calculated correctly and to anticipate if in the future the start_time or end_time can change at any time.
    
    BREAKING CHANGE: `duration` attribute will by default be stored as 0

--- app/models/user/sleep.rb
+++ app/models/user/sleep.rb
@@ -2,4 +2,18 @@ class User::Sleep < ApplicationRecord
   validates :start_time, presence: true
 
   belongs_to :user
+
+  before_save :set_duration
+
+  private
+
+  def set_duration
+    duration = 0
+
+    if start_time.present? && end_time.present?
+      duration = end_time - start_time
+    end
+
+    self.duration = duration
+  end
 end
