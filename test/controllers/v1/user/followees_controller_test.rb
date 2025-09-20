7402012333b1eda695e164fc033fa0b72d493a62
Yusuf Manshur <ymanshur@gmail.com>
2025-09-21 05:32:31 +0700

    test(user/followees_controller): basic controller suite including index, show, create (follow) and destroy (un-follow) action

---
+++ test/controllers/v1/user/followees_controller_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class V1::User::FolloweesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @follows = follows(:one)
+    @user = @follows.follower
+    @followee = @follows.followed
+
+    @followed = users(:three)
+  end
+
+  test "should get index" do
+    get v1_user_followees_url(@user), as: :json
+    assert_response :success
+  end
+
+  test "should create user_followee" do
+    assert_difference("Follow.count") do
+      post v1_user_followees_url(@user), params: { followed_id: @followed.id }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should show user_followee" do
+    get v1_user_followee_url(@user, @followee), as: :json
+    assert_response :success
+  end
+
+  test "should destroy user_followee" do
+    assert_difference("Follow.count", -1) do
+      delete v1_user_followee_url(@user, @followee), as: :json
+    end
+
+    assert_response :no_content
+  end
+end
