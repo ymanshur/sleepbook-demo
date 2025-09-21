0dc4ed02023cc6da40cc16752a3899a3148d3869
Yusuf Manshur <ymanshur@gmail.com>
2025-09-22 05:17:43 +0700

    feat(follow): should not follow yourself

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,5 +2,12 @@ class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
 
-  validates :follower, uniqueness: { scope: :followed, message: "cannot follow same user twice" }
+  validates :followed, uniqueness: { scope: :follower }
+  validate :must_not_yourself
+
+  private
+
+  def must_not_yourself
+    errors.add(:followed, "must not yourself") if follower_id == followed_id
+  end
 end
