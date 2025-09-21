45c4045b8d05895a6dc23a7b0852ac71169c33b6
Yusuf Manshur <ymanshur@gmail.com>
2025-09-22 05:17:24 +0700

    test(follow): should not follow yourself

--- test/models/follow_test.rb
+++ test/models/follow_test.rb
@@ -3,10 +3,16 @@ require "test_helper"
 class FollowTest < ActiveSupport::TestCase
   setup do
     @follow = follows(:one)
+    @user = users(:one)
   end
 
   test "should not follow same user twice" do
     new_follow = Follow.new(follower: @follow.follower, followed: @follow.followed)
     assert_not new_follow.save, "Followed the same user twice"
+  end
+
+  test "should not follow yourself" do
+    new_follow = Follow.new(follower: @user, followed: @user)
+    assert_not new_follow.save, "Followed yourself"
   end
 end
