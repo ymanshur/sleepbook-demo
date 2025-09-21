4ab44f40e64b7218eafaf624a672fe2965d5bec5
Yusuf Manshur <ymanshur@gmail.com>
2025-09-21 15:18:15 +0700

    feat(models/user): leverage has_many :through Model association to get all sleeps from user's followees
    The query produced consist of nested (3) INNER JOIN between user_sleeps, users, and follows table. The users table included to make sure the following's reference is valid, but it might be optimized to only joins user_sleeps and follows table with action user authorization before.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :sleeps, dependent: :destroy
   has_many :follows, foreign_key: :follower_id, dependent: :destroy
   has_many :followees, through: :follows, source: :followed
+  has_many :followees_sleeps, through: :followees, source: :sleeps
   has_many :being_follows, class_name: "Follow", foreign_key: :followed_id, dependent: :destroy
   has_many :followers, through: :being_follows, source: :follower
 
