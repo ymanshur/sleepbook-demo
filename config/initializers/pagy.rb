98a867367215c261161a38a4a92463c1d54e17b5
Yusuf Manshur <ymanshur@gmail.com>
2025-09-21 16:53:10 +0700

    chore(pagy): set limit max by ENV
    Since the scope limit read ENV, It makes sense that the pagy default limit is also the same.
    
    References: https://ddnexus.github.io/pagy/docs/extras/limit

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,7 +1,10 @@
 
-Pagy::DEFAULT[:limit] = 25 # items per page
 Pagy::DEFAULT[:count_args] = :id
 
-require "pagy/extras/metadata"
+require "pagy/extras/limit"
+Pagy::DEFAULT[:limit_max] = ENV.fetch("DEFAULT_SCOPE_LIMIT", 25).to_i
+
 require "pagy/extras/overflow"
 Pagy::DEFAULT[:overflow] = :last_page
+
+require "pagy/extras/metadata"
