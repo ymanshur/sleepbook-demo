c432a5631948a4fcc5561feaa55e8bcaeb1e5226
Yusuf Manshur <ymanshur@gmail.com>
2025-09-22 22:19:58 +0700

    fix(db/migrate): rollback compatible

--- db/migrate/20250921155004_add_composite_index_to_user_sleeps.rb
+++ db/migrate/20250921155004_add_composite_index_to_user_sleeps.rb
@@ -1,5 +1,9 @@
 class AddCompositeIndexToUserSleeps < ActiveRecord::Migration[8.0]
-  def change
+  def up
     add_index :user_sleeps, [ :user_id, :start_time, :duration ]
+  end
+
+  def down
+    remove_index :user_sleeps, [ :user_id, :start_time, :duration ], if_exists: true
   end
 end
--- db/migrate/20250921175256_remove_composite_index_from_user_sleeps.rb
+++ db/migrate/20250921175256_remove_composite_index_from_user_sleeps.rb
@@ -1,5 +1,5 @@
 class RemoveCompositeIndexFromUserSleeps < ActiveRecord::Migration[8.0]
-  def change
+  def up
     remove_index :user_sleeps, name: :index_user_sleeps_on_user_id_and_start_time_and_duration
   end
 end
