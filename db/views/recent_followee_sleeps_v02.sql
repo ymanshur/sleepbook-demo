7f01fca7afc47dd561f1caeeb80e708ead73022f
Yusuf Manshur <ymanshur@gmail.com>
2025-09-25 05:49:48 +0700

    perf(views/recent_followee_sleeps): add sleep start_time and end_time to the view
    So when to return sleep data, does not require eager load of sleep tables

--- db/views/recent_followee_sleeps_v02.sql
+++ db/views/recent_followee_sleeps_v02.sql
@@ -3,13 +3,14 @@ SELECT
     f.follower_id,
     us.id AS sleep_id,
     us.user_id,
+    us.start_time,
+    us.end_time,
     us.duration
 FROM
     follows f
-    JOIN users u ON f.followed_id = u.id
-    JOIN user_sleeps us ON us.user_id = u.id
+    JOIN user_sleeps us ON us.user_id = f.followed_id
 WHERE
     us.end_time IS NOT NULL
-    AND us.start_time >= date_trunc('week', NOW() - INTERVAL '1 week')
+    AND us.start_time >= DATE_TRUNC('week', NOW() - INTERVAL '1 week')
 ORDER BY
     us.duration DESC;
